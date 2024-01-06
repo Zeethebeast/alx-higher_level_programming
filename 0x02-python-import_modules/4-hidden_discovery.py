0721b032805e375103e52d15aba67019b7b7b9ab
Zeethebeast <chimasamuel6@gmail.com>
2024-01-05 23:32:59 -0800

    task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     all_functions = dir()
     for i in range(0, len(all_functions)):
-        if all_functions[1][:2] != "__":
+        if all_functions[i][:2] != "__":
             print("{:s}".format(all_functions[i]))
