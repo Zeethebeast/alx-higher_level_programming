e360bd8e4e02685119d801843b12d11734babe12
Zeethebeast <chimasamuel6@gmail.com>
2024-01-08 17:58:20 -0800

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env/python3
+def no_c(my_string):
+    if my_string[:]:
+        new_string = my_string.translate(str.maketrans({"c": None}))
+        string_2 = new_string.translate(str.maketrans({"C": None}))
+        return string_2
+    return my_string
