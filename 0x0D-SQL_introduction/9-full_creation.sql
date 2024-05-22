f19cada65ee50ed2d73daa8d36e1c8d9b6b934ab
zeethebeast <chimasamuel6@gmail.com>
2024-05-22 16:52:10 -0700

    my sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- This scripts creates a second table in the datatbse
+-- This scripts creates a second table in the datatbase
 CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
