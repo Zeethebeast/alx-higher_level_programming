edc1bbbb85f610ed79d624d2138949fffc181342
zeethebeast <chimasamuel6@gmail.com>
2024-05-23 02:01:14 -0700

    mysql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,13 @@ CREATE TABLE IF NOT EXISTS second_table(
 -- inserts data into the second table
 INSERT INTO second_table (id, name, score)
 VALUES
-	(1, 'John', 10), 
-	(2, 'Alex', 10), 
-	(3, 'Bob', 14), 
+	(1, 'John', 10);
+INSERT INTO second_table (id, name, score)
+VALUES
+	(2, 'Alex', 10);
+INSERT INTO second_table (id, name, score)
+VALUES
+	(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score)
+VALUES
 	(4, 'George', 8);                                                                         
