33d8169d829fb6045f76dd977d175ba37d360a31
zeethebeast <chimasamuel6@gmail.com>
2024-06-19 10:41:07 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const lin = [
+'C is fun',
+'Python is cool',
+'JavaScript is amazing'
+];
+
+let index = 0;
+
+while (index < lin.length) {
+	console.log(lin[index]);
+	index++;
+}
