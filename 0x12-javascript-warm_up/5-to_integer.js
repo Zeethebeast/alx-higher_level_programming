feae5bd36e80be736946afa48a69506ab4942a2a
zeethebeast <chimasamuel6@gmail.com>
2024-06-19 09:49:42 -0700

    javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,12 +2,12 @@
 const args = process.argv;
 
 if (!args[2]) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	const num = parseInt(args[2], 10);
-	if (isNaN(num)) {
-		console.log('Not a number');
-	} else {
-		console.log('My number: ' + num);
-	}
+  const num = parseInt(args[2], 10);
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + num);
+  }
 }
