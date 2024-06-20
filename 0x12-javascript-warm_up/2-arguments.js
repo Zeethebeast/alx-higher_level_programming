2995bee27b16a936190c7ffb9ce14541b36bac0a
zeethebeast <chimasamuel6@gmail.com>
2024-06-19 19:13:30 -0700

    script2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-
 const args = process.argv.slice(2);
 
-if (!args[0]) {
-	let no = `No argument`;
-	console.log(no);
-} else if (args[1]) {
-	//let one = `Argument found`;
-	console.log('Arguments found');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
 } else {
-	//let many = `Arguments found`;
-	console.log('Argument found');
-};
+  // let many = `Arguments found`;
+  console.log('Arguments found');
+}
