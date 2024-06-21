78772d54c1174ba5e749d67d231d66813c00221e
zeethebeast <chimasamuel6@gmail.com>
2024-06-21 05:44:47 +0100

    task1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-			return;
-		}
-		this.width = w;
-		this.height = h;
-		// this.print();
-	}
+  constructor (w, h) {
+    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
 
-	print () {
-		for (let index = 0; index <= this.height; index++) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  print () {
+    for (let index = 0; index <= this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 }
 module.exports = Rectangle;
