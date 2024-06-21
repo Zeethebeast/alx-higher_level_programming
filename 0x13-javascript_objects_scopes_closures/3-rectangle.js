f637887e867853c4cf6c8cf90397dc4c01d1a8a5
Ekwensu <chimasamuel6@gmail.com>
2024-06-21 04:56:53 +0100

    objects

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Rectangle {
+	constructor(w, h) {
+		 if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
+            return;
+		}
+		this.width = w;
+		this.height = h;
+		//this.print();
+		}
+		print() {
+			for (let index = 0; index <= this.height; index++){
+				console.log('X'.repeat(this.width));
+			}
+		}
+
+}
+module.exports = Rectangle;
