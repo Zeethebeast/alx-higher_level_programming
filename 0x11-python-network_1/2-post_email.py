c15a954f0dec1820760790ad14ee530ca10273ad
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 14:55:20 -0700

    python network

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import urllib.parse
+import urllib.request
+import sys
+
+
+url = 'http://0.0.0.0:5000/post_email'
+values = {'email' : 'hr@holbertonschool.com'}
+data = urllib.parse.urlencode(values)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    response_data = response.read().decode('utf-8')
+
+print(f'Your email is: {response_data}')
