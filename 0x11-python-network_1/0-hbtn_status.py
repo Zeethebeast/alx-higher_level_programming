cc19f493e0baf8794b59b5b5ce9b05ecc0d65bdd
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 07:20:29 -0700

    python networking

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+
+print('Body response:')
+print(f'\t-type: {type(html)}')
+print(f'\t-content: {html}')
+print(f'\t- utf8 content: {html.decode("utf-8")}')
