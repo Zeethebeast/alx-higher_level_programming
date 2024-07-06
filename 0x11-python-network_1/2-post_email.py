f7e009dbb343a0a023f38267cb764d36de2163a1
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 18:10:47 -0700

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
+# -*- coding: utf-8 -*-
+"""This is a python script take in an url and displays"""
+"""In the format or manner presented below"""
 
-
-url = 'http://0.0.0.0:5000/post_email'
-values = {'email': 'hr@holbertonschool.com'}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    response_data = response.read().decode('utf-8')
-
-print(f'Your email is: {response_data}')
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    
+    url = 'http://0.0.0.0:5000/post_email'
+    values = {'email': 'hr@holbertonschool.com'}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        response_data = response.read().decode('utf-8')
+        print(f'Your email is: {response_data}')
