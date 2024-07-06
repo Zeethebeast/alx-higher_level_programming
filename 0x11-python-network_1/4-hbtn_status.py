e0f4dbad4488712ad0b3447d7123d758bffcf524
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 19:07:11 -0700

    tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read().decode('utf-8')
+"""
+Script to fetch and print the body response from specified URLs using the requests module.
+"""
+import requests
 
-print('Body response:')
-print(f'\t- type: {type(html)}')
-print(f'\t- content: {html}')
+# Fetch data from https://alx-intranet.hbtn.io/status
+url1 = 'https://alx-intranet.hbtn.io/status'
+response1 = requests.get(url1)
+html1 = response1.text
+
+print('Body response from', url1)
+print(f'\t- type: {type(html1)}')
+print(f'\t- content: {html1}')
+
+# Fetch data from http://0.0.0.0:5050/status
+url2 = 'http://0.0.0.0:5050/status'
+response2 = requests.get(url2)
+html2 = response2.text
+
+print('Body response from', url2)
+print(f'\t- type: {type(html2)}')
+print(f'\t- content: {html2}')
