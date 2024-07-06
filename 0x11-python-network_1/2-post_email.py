b9279baba97fe302aae72cf12e9087d02d77096f
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 18:16:45 -0700

    tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
     import sys
     
     url = 'http://0.0.0.0:5000/post_email'
-    values = {'email': 'hr@holbertonschool.com'}
+    values = {}
+    values['email'] = sys.argv[2]
+
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
+    req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         response_data = response.read().decode('utf-8')
         print(f'Your email is: {response_data}')
