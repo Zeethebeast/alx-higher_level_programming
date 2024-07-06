9ba28c83b5405327b2d356a9c9a2935fd944a1da
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 19:13:28 -0700

    tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,15 +9,6 @@ url1 = 'https://alx-intranet.hbtn.io/status'
 response1 = requests.get(url1)
 html1 = response1.text
 
-print('Body response from', url1)
+print('Body response:')
 print(f'\t- type: {type(html1)}')
 print(f'\t- content: {html1}')
-
-# Fetch data from http://0.0.0.0:5050/status
-url2 = 'http://0.0.0.0:5050/status'
-response2 = requests.get(url2)
-html2 = response2.text
-
-print('Body response from', url2)
-print(f'\t- type: {type(html2)}')
-print(f'\t- content: {html2}')
