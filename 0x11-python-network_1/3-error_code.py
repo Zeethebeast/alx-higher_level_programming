63db71fd890a3a6bbf13c06d24f601180952fe12
zeethebeast <chimasamuel6@gmail.com>
2024-07-05 18:33:36 -0700

    tasks

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,13 +2,23 @@
 import sys
 import urllib.request
 import urllib.error
-url = sys.argv[1]
-req = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(req) as response:
-        response_data = response.read().decode('utf-8')
-        print(response_data)
-except urllib.error.HTTPError as e:
-    print('Error code: ', e.code)
-except urllib.error.URLError as e:
-    print('Reason: ', e.reason)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            response_data = response.read().decode('utf-8')
+            print(response_data)
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
+    except urllib.error.URLError as e:
+        if hasattr(e, 'reason'):
+            print('Reason: ', e.reason)
+        else:
+            print('URLError occurred, reason not available.')
