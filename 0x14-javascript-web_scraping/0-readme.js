be5032bb5a580d0ab4969ff074a298fcf3c72656
zeethebeast <chimasamuel6@gmail.com>
2024-07-31 04:10:21 +0100

    js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,20 @@
+#!/usr/local/bin/node
+const fs = require('fs').promises;
+
+async function readFileContent(filepath) {
+    try {
+        const data = await fs.readFile(filepath, 'utf-8');
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const filepath = process.argv[2];
+
+if (!filepath) {
+    console.error('Provide a file path as argument');
+    process.exit(1);
+}
+
+readFileContent(filepath);
